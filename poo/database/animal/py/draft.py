6ac9c6e89059ffcbdfbd3b2f13c0ca2ea8d71184
yanomaduarte <yanduarte13@gmail.com>
2025-09-20 14:48:42 -0300

    Atividade Animal com construtor corrigido e makeSound implementeado

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,20 +1,22 @@
 class Animal:
     def __init__(self, species: str, age: int, sound: str):
         self.species: str = species
-        self.age: int = 0
         self.sound: str = sound
-    
-    def makeSound(self, ) -> None:
-        print(self.sound)
+        self.age: int
 
-    def ageBy(self) -> int:
-        if self.age == 1:
-            return 1
-        if self.age == 2:
-        
+    def Animal(self, species: str, sound: str):
+        self.species = species
+        self.sound = sound
 
-    def show(self) -> None:
-        print(f"Species: {self.species}, Age: {self.age}, Sound: {self.sound}") 
-    
-    def __str__(self) -> str:
-        return f"{self.species}:{self.age}:{self.sound}." 
+    def makeSound(self) -> str:
+        if(self.age == 0):
+            return "---"
+
+        if(self.age == 4):
+            return "RIP"
+
+        return self.sound
+    def ageBy(self, age: int) -> None:
+        if(self.age + age >= 4):
+            self.age = 4
+            print(f"warning: {self.species} morreu")
