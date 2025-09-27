d977fad8d630d055975c223db7d324723b8b0975
¨yanduartePC¨ <yanduarte13@gmail.com>
2025-09-27 14:56:31 -0300

    questCarro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -8,11 +8,18 @@ class Car:
 
     def __str__(self):
         return f"pass:{self.pass_}, gas:{self.gas}, km:{self.km}"
-    
+
     def show(self):
         print(self.__str__())
 
     def enter(self):
+        if self.pass_ < self.passMax:
+            self.pass_ += 1
+        else:
+            print("fail: limite de pessoas atingido.")
 
-
-    
+    def leave(self):
+        if self.pass_ > 0:
+            self.pass_ -= 1
+        else:
+            print("fail: não há ninguem no carro.")
