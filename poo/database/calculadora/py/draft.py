0d34b4ff397bc87299757e2bb2c126be09765dfd
¨yanduartePC¨ <yanduarte13@gmail.com>
2025-09-29 17:39:57 -0300

    questCalculadora solved

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -17,12 +17,29 @@ class Calculator:
         print(self.__str__())
 
     def charge(self, increment):
-        if increment > 0:
+        if increment < 0:
             return
         self.battery += increment
-
         if self.battery > self.batteryMax:
             self.battery = self.batteryMax
+
+    def sum(self, a: float, b: float):
+        if self.battery == 0:
+            print("fail: bateria insuficiente")
+            return
+        self.display = a + b
+        self.battery -= 1
+
+    def div(self, a: float, b: float):
+        if self.battery == 0:
+            print("fail: bateria insuficiente")
+            return
+        if b == 0:
+            print("fail: divisao por zero")
+            self.battery -= 1
+            return
+        self.display = a / b
+        self.battery -= 1
 
 
 def main():
@@ -41,6 +58,14 @@ def main():
         elif args[0] == "charge":
             inc = int(args[1])
             calc.charge(inc)
+        elif args[0] == "sum":
+            a = float(args[1])
+            b = float(args[2])
+            calc.sum(a, b)
+        elif args[0] == "div":
+            a = float(args[1])
+            b = float(args[2])
+            calc.div(a, b)
         else:
             print("fail: comando invalido")
 
